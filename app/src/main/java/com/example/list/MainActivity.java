25cc645733d669ace09be125bcdbd6f4741ccdda
Aleksey Iskovskikh <iskovskikh56@gmail.com>
2020-03-03 12:06:21 +0500

    дороботки

--- app/src/main/java/com/example/list/MainActivity.java
+++ app/src/main/java/com/example/list/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.list;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
@@ -9,7 +8,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
@@ -22,53 +20,54 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView listView;
     private SharedPreferences sharedPrefTxt;
-    private static String LARGE_TEXT = "large_text";
-    private List<Map<String, String>> contentList;
-    private BaseAdapter listContentAdapter;
     private SwipeRefreshLayout swipeLayout;
-    private ArrayList<Integer> indexDelItems = new ArrayList<>();
+    private static  String LARGE_TEXT = "large_text";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SharedPreferences preferences = getApplication().getSharedPreferences("SHARED_PREFS_KEY", MODE_PRIVATE);
-        String text = preferences.getString("saved_text", getString(R.string.large_text));
-        preferences.edit().putString("saved_text", text).apply();
-        prepareContent();
-
+        listView = findViewById(R.id.list);
+        sharedPrefTxt = getApplication().getSharedPreferences("SHARED_PREFS_KEY", MODE_PRIVATE);
+        String text = sharedPrefTxt.getString(LARGE_TEXT, getString(R.string.large_text));
+        sharedPrefTxt.edit().putString(LARGE_TEXT, text).apply();
+        paperContent();
     }
 
-    @NonNull
-    private String[] prepareContent() {
+
+    private void paperContent(){
         String[] arrayContent = sharedPrefTxt.getString(LARGE_TEXT, "").split("\n\n");
-        return arrayContent;
-    }
+        List<Map<String, String>> arrayList= new ArrayList<>();
+        Map<String, String> map;
+        for (String value: arrayContent){
+            map = new HashMap<>();
+            map.put("text", value);
+            map.put("lenght", Integer.toString(value.length()));
+            arrayList.add(map);
 
-
-    @NonNull
-    private SimpleAdapter createAdapter(String[] stringsTxt) {
-        contentList = new ArrayList<>();
-        prepareAdapterContent(stringsTxt);
-        return new SimpleAdapter(this, contentList, R.layout.list_layout,
-                new String[]{"text", "lenght"},
-                new int[]{R.id.txtText, R.id.txtNumber});
-    }
-
-
-    @NonNull
-    private List<Map<String, String>> prepareAdapterContent(String[] stringsTxt) {
-        Map<String, String> mapList;
-        for (String value : stringsTxt) {
-            mapList = new HashMap<>();
-            mapList.put("text", value);
-            mapList.put("lenght", Integer.toString(value.length()));
-            contentList.add(mapList);
         }
-        ;
-        return contentList;
+
+        final SimpleAdapter adapter = new SimpleAdapter(this, arrayList, R.layout.list_layout,
+                new String[]{"text","lenght"},
+                new int[]{R.id.txtText, R.id.txtNumber});
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String str = ((TextView)view).getText().toString();
+               
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+//        swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+//            @Override
+//            public void onRefresh() {
+//            }
+//        });
     }
 }
-
